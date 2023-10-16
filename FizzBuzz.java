6bacc9337ee31664b9e6f6939a73622740635afd
Jiang <zhou.jiang@fiserv.com>
2023-10-16 15:58:19 -0400

    add FizzBuzz.java as solution to for the FizzBuzz problem and the resursive form.

---
+++ FizzBuzz.java
@@ -0,0 +1,39 @@
+public class FizzBuzz{
+    public static void main(String[] args) {
+        int n = 15;
+        fizzBuzz(n);
+        recursive(n, 1);
+    }
+
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i<=n; i++) {
+            if (i % 3 ==0 && i % 5 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 ==0 ) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void recursive(int n, int counter) {
+        if(counter <= n) {
+            if (counter % 3 ==0 && counter % 5 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (counter % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (counter % 3 ==0 ) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(counter);
+            }
+            recursive(n, counter +1);
+        } else {
+            return;
+        }
+    }
+}
